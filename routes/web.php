cd0f1652ba2f33570924b42d22e6e8e31a65634e
FAYSAL AHMED BHUIYAN <147671727+faysalahmedbhuiyan@users.noreply.github.com>
2026-04-30 15:34:31 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration done';
+});
 Route::get('/', function () {
     return response()->json([
         'status' => true,
