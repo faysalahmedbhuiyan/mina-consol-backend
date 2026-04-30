e5708aacc7f83be1afc4e18bf67838bd4db3a1a2
FAYSAL AHMED BHUIYAN <147671727+faysalahmedbhuiyan@users.noreply.github.com>
2026-04-30 15:36:31 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/migrate', function () {
     Artisan::call('migrate', ['--force' => true]);
     return 'Migration done';
 });
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage linked';
+});
 Route::get('/', function () {
     return response()->json([
         'status' => true,
