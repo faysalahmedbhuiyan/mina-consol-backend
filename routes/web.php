d21e5a95f13599d6c4dc69218b6f98ddc93468c5
Md Faysal Ahmed Bhuiyan <faysalahmedtushan@gmail.com>
2026-05-02 16:12:16 +0900

    fix: session + storage

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,52 @@ Route::get('/', function () {
         'message' => 'Backend is running',
     ]);
 });
+
+//for test railway
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/clear', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    return 'Cleared';
+});
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration done';
+});
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage linked';
+});
+Route::get('/', function () {
+    return response()->json([
+        'status' => true,
+        'message' => 'Backend is running',
+    ]);
+});
+Route::get('/debug', function () {
+    return [
+        'app_key' => config('app.key'),
+        'db' => config('database.default'),
+        'db_path' => config('database.connections.sqlite.database'),
+        'storage_writable' => is_writable(storage_path()),
+        'cache_writable' => is_writable(base_path('bootstrap/cache')),
+    ];
+});
+Route::get('/fix-storage', function () {
+    $paths = [
+        storage_path('framework/sessions'),
+        storage_path('framework/views'),
+        storage_path('framework/cache'),
+    ];
+
+    foreach ($paths as $path) {
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+
+    return 'Storage fixed';
+});
