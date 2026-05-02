f3b8df57f4351e9c81c0262f94eb0e8a7a25aee6
Md Faysal Ahmed Bhuiyan <faysalahmedtushan@gmail.com>
2026-05-02 21:01:30 +0900

    add railway debug routes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,20 @@ Route::get('/', function () {
         'message' => 'Backend is running',
     ]);
 });
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration done';
+});
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage linked';
+});
+
+Route::get('/debug', function () {
+    return [
+        'db' => config('database.default'),
+    ];
+});
